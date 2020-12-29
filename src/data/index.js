8252bfed8e9c667d48d5533012af57bb71bcfbe5
Archie <archie@nativ3.io>
2020-12-29 17:20:49 -0600

    create dummy data

---
+++ src/data/index.js
@@ -0,0 +1,56 @@
+const Items = [
+  {
+    name: 'Widget 1',
+    price: 100,
+    dimension: {
+      width: 11,
+      height: 12,
+      length: 13
+    },
+    weight: 14,
+    capacitor: '10L',
+    image: 'https://via.placeholder.com/800x600',
+    description: '<h3>Subtitle goes here</h3>\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  },
+  {
+    name: 'Widget 2',
+    price: 200,
+    dimension: {
+      width: 21,
+      height: 22,
+      length: 23
+    },
+    weight: 24,
+    capacitor: '10L',
+    image: 'https://via.placeholder.com/800x600',
+    description: '<h3>Subtitle goes here</h3>\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  },
+  {
+    name: 'Widget 3',
+    price: 300,
+    dimension: {
+      width: 31,
+      height: 32,
+      length: 33
+    },
+    weight: 34,
+    capacitor: '10L',
+    image: 'https://via.placeholder.com/800x600',
+    description: '<h3>Subtitle goes here</h3>\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  },
+  {
+    name: 'Widget 4',
+    price: 400,
+    dimension: {
+      width: 41,
+      height: 42,
+      length: 43
+    },
+    weight: 44,
+    capacitor: '10L',
+    image: 'https://via.placeholder.com/800x600',
+    description: '<h3>Subtitle goes here</h3>\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  },
+];
+
+export default Items;
